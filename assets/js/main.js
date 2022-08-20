6035c2ead6afd9f26575be4d37b7d67ac477756c
mathalav55 <matalav55@gmail.com>
2022-08-20 20:49:52 +0530

    added media queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,17 +83,96 @@ closeBtns.forEach((closeBtn,i)=>{
 })
 
 // Swiper
-let swiper = new Swiper(".portfolio-container", {
+let swiperPortfolio = new Swiper(".portfolio-container", {
     cssMode: true,
     loop : true,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     mousewheel: true,
     keyboard: true,
-  });
+});
+
+var swiper = new Swiper('.testimonial-container', {
+    loop : true,
+    grabCursor: true,
+    spaceBetween: 48,
+    pagination: {
+        el: '.swiper-pagination-testimonial',
+        dynamicBullets: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView:2
+        }
+    }
+});
+
+// Active Link
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    });
+}
+window.addEventListener('scroll', scrollActive);
+// change header
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader);
+
+// show scroll top
+function showScrollTop(){
+    const scrollTop = document.querySelector('.scroll-up');
+    if(this.scrollY >= 300) scrollTop.classList.add('show-scroll-top'); else scrollTop.classList.remove('show-scroll-top');
+}
+window.addEventListener('scroll', showScrollTop);
+
+// dark theme
+const themeButton = document.querySelector('.theme-icon')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
